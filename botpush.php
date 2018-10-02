89e8e2230294f615470859990160af8232ac2b88
ets9tk1234 <43776406+ets9tk1234@users.noreply.github.com>
2018-10-02 16:07:35 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'fG1iHH7gHsyP6MmfQLf8kcFABPsLJnf5ypgrJKU/VE2fkrskAJNpNaBpfuzT451PmVO+pdRDdiObfFtQb2Ax827X2N3Q/NuhlcMQ6s8oLBh1tHos6q7QHXywsKoyZgYPjHyMm6JaS9U5LYitTHDIAgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '97537402860a77d7334049fec6a8bedf';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
